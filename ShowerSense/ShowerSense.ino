5b7a46a7ce5ea13e7a477eea3ddb93596c7c82da
Sean Connell <seanrobertconnell@gmail.com>
2013-02-22 00:19:56 -0800

    Reimplemented as a more general State Machine.
    Ripped out old hackey code with a more proper first cut an extensible
    state machine skeleton to allow adding in other sensors later. Need to
    test on hardware.

--- ShowerSense/ShowerSense.ino
+++ ShowerSense/ShowerSense.ino
@@ -1,159 +1,128 @@
 /*
-Written by Sean Connell (Hacked together is a better description) on 5/13/2012, and then rewritten with different sensors on 8/13 for Float On.
-Uses demo code from LadyAda's flowrate sensor test
-Detects shower usage by temperature gradiant and sound level thresholding. Indicates this via three LEDs.
-This allows the pump operator to begin running the pumps before the floater has even left the room.
+       File: ShowerSense.ino 
+    Purpose: Sense waterflow and alert a listener
+Application: Float on shower usage detector informing a node that signals the Helm node. Eventual use might be also automatically reading sensors in the tank.
+     Author: Sean Connell
 */
 
-//hook up some indicator leds to these pins
-#define LED1 9
-#define LED2 10
-#define LED3 11
-#define FLOW_THRESH 50
-#define SMOOTHING_DELAY 2
+#define IN_USE_FLOW_RATE 50
+#define TIMER_PERIOD 1000
+#define SHOWER_IN_USE(X) (return X > IN_USE_FLOW_RATE)
+#define FLOW_SENSOR_PIN 2
+#define CLEAR_PENDING_INTERRUPTS EIFR = 0
+#define ENABLE_INTERRUPTS sei()
+#define DISABLE_INTERRUPTS cli()
 
-// pin to use for reading the flow rate sensor
-#define FLOWSENSORPIN 2
+//States for the system
+#define INITIAL         B00000000
+#define DATA_TRANSFORM  B00000001
+#define NOTIFY_LISTENER B00000010
+#define DATA_GATHERING  B00000100
+#define CLEAR_STATE     B00001000
+#define RESTART  B10000000
 
-//Led brightness for the animation
-int brightness1 = 0;    // how bright the LED is
-int brightness2 = 83;
-int brightness3 = 166;
-//Animation scheme is constant brightness amount, different time between changes
-int fadeAmount1 = 1;    // Direction to fade LEDs
-int fadeAmount2 = 1;
-int fadeAmount3 = 1;
-//Use a logarythmic lookup table to keep the fade smooth by only increasing the brightness 1 each time, but varying the amount of time between increase/decreases
-int fadeTable[] = {  1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
-int di = 0; //index variable for fadetable
-#define FADE_LENGTH 256 //length of table
+//Volatile stateful variables
+volatile uint16_t  pulse_count;
+volatile uint8_t STATE = INITIAL;
 
-//the end goal is to set this when somebody is showering
-int showerInUse = 0;
-//Tells us whether to fade leds up or continue animating
-boolean onTransition = 1;
-//LadyAda's app note for reading the sensor with a constant timed interrupt
-volatile uint16_t pulses = 0; // count how many pulses!
-volatile uint8_t lastflowpinstate; // track the state of the pulse pin
-volatile uint32_t lastflowratetimer = 0; // you can try to keep time of how long it is between pulses
-volatile float flowrate; // and use that to calculate a flow rate
-// Interrupt is called once a millisecond, looks for any pulses from the sensor!
-SIGNAL(TIMER0_COMPA_vect) {
-  uint8_t x = digitalRead(FLOWSENSORPIN);
-  
-  if (x == lastflowpinstate) {
-    lastflowratetimer++;
-    return; // nothing changed!
+//Data to be transformed register
+#define FLOW_RATE_DATA  B00000001
+#define SALINITY_DATA   B00000010
+#define PH_DATA         B00000100
+
+//less state critical variables
+uint16_t flow_rate = 0;
+
+//The data gathering window has ended and we should move on to calculating reporting values
+ISR(TIMER1_COMPA_vect){
+  if(STATE != DATA_GATHERING){
+    /* We somehow got to the end of our data gathering window without resetting state
+    Might be a bad calc somewhere in the DATA_TRANSFORM step
+    Try to restart and hope that it doesn't happen again */ 
+    STATE = RESTART; 
+    DISABLE_INTERRUPTS;//We're going to muck around with state, don't want to get interrupted
   }
-  
-  if (x == HIGH) {
-    //low to high transition!
-    pulses++;
-  }
-  lastflowpinstate = x;
-  flowrate = 1000.0;
-  flowrate /= lastflowratetimer;  // in hertz
-  lastflowratetimer = 0;
-}
-
-void useInterrupt(boolean v) {
-  if (v) {
-    // Timer0 is already used for millis() - we'll just interrupt somewhere
-    // in the middle and call the "Compare A" function above
-    OCR0A = 0xAF;
-    TIMSK0 |= _BV(OCIE0A);
-  } else {
-    // do not call the interrupt function COMPA anymore
-    TIMSK0 &= ~_BV(OCIE0A);
+  else{
+    STATE = DATA_TRANSFORM;
+    //TODO should also stop timer here for the best accuracy
+    DISABLE_INTERRUPTS;//Don't want to collect any data while we copy some values
   }
 }
 
 void setup() {
-   // initialize serial communication with computer
-   Serial.begin(115200);  
-   pinMode(FLOWSENSORPIN, INPUT);
-   digitalWrite(FLOWSENSORPIN, HIGH);
-   lastflowpinstate = digitalRead(FLOWSENSORPIN);
-   useInterrupt(true);
-   // LEDs are outputs
-   pinMode(LED1, OUTPUT);
-   pinMode(LED2, OUTPUT);
-   pinMode(LED3, OUTPUT);
+   Serial.begin(115200); //initialize uart
+   pinMode(FLOW_SENSOR_PIN, INPUT); 
+   digitalWrite(FLOW_SENSOR_PIN, HIGH);
+   attachInterrupt(FLOW_SENSOR_PIN, pulses_counter, CHANGE);
+   initialize_timer();
+   STATE = DATA_GATHERING;
+   ENABLE_INTERRUPTS;
 }
 
 void loop()
 { 
-  Serial.print("Freq: "); Serial.print(flowrate);
-  Serial.print("\tIn Use: "); Serial.println(showerInUse);
-
-  if(flowrate > FLOW_THRESH){//Threshold exceeded, shower is being used
-      showerInUse = 1;
-    }
-  else{//Shower not in use
-      showerInUse = 0;
-    }
-  if(showerInUse){
-      if(onTransition){ //just started being used, fade up the LEDs
-          LEDFadeUp(); 
-          onTransition = 0; 
-      }
-      LEDAnimate(); //LEDs have faded into appropriate brightness, animate!
-    }//Send the signal for In Use
-  else{//Turn LEDs off, shower not in use
-      LEDFadeDown();//return to off smoothly
-      onTransition = 1;//get ready to fade up next use
+  /* State machine transitions and actions that 
+  aren't time critical or are too heavy to do
+  in interrupts are done here. */
+  switch(STATE){
+    
+    //Not concerned with DATA_GATHERING 
+    case DATA_GATHERING:
+      break;
+      
+    //If the data is ready to process do the neccesary
+    //steps to transform it into output form
+    case DATA_TRANSFORM:
+      flow_rate = calculate_flow_rate(pulse_count);
+      STATE = DATA_GATHERING
+      ENABLE_INTERRUPTS;
+      break;
+      
+    
+    //Notify whatever is listening by whatever method
+    case NOTIFY_LISTENER:
+      Serial.print("Flow Rate:\t"); 
+      Serial.println(flow_rate);
+      STATE = CLEAR_STATE;
+      break;
+    
+    //reset everything 
+    case CLEAR_STATE:
+      flow_rate = 0;
+      CLEAR_PENDING_INTERRUPTS;
+      STATE = DATA_GATHERING;
+      ENABLE_INTERRUPTS;
+      break;
+      
+    //currently hard coded error message for reset. Generalize later
+    case RESTART:
+      Serial.println("Reset not yet implemented. Would if it could though...");
+      Serial.println("|-------------------------------------RESETTING------------------------------------|"); 
+      Serial.println("| ILLEGAL STATE: Did not return to DATA_GATHERING state before time window elapsed |"); 
+      Serial.println("|-------------------------------------RESETTING------------------------------------|"); 
+      //reset here
+      break;
   }
-  //float liters = pulses;
-  //liters /= 7.5; // liters/s
-  delay(10);//chill a little while
 }
 
-void LEDFadeUp(){//fades LEDs down to initial brightness for a smoother transition. I know it's blocking, too bad.
-    analogWrite(LED1, 0);//this guy is just off
-    while(brightness2 < 83){
-        brightness2++; 
-        brightness3++; 
-        analogWrite(LED2, brightness2);
-        analogWrite(LED3, brightness3);
-        delay(SMOOTHING_DELAY);}
-    while(brightness3 < 166){
-        brightness3++; 
-        delay(SMOOTHING_DELAY);
-    }
+//State safe counter
+void pulses_counter(){
+  if(STATE == DATA_GATHERING){
+    pulse_count++;
+  }
 }
 
-void LEDFadeDown(){//fades LEDs down to no brightness for a smoother transition. I know it's blocking, too bad.
-    while((brightness1+brightness2+brightness3) > 0){
-        if(brightness1 >0 ){brightness1--; analogWrite(LED1, brightness1);}
-        if(brightness2 >0 ){brightness2--; analogWrite(LED2, brightness2);}
-        if(brightness3 >0 ){brightness3--; analogWrite(LED3, brightness3);}
-        delay(SMOOTHING_DELAY);
-    }
+void initialize_timer(){
+  TCCR1A = 0;
+  TCCR1B = 0;
+  OCR1A = 46874; //compare mode with 3 second intervals at this clock rate of 16mhz
+  TCCR1B |= (1 << WGM12); //CTC mode
+  TCCR1B |= (1 << CS10) | (1 << CS12); //Set 1024 prescaler bits
+  TIMSK1 |= (1 << OCIE1A); //enable timer compare interrupt
 }
 
-void LEDAnimate()  { 
-    analogWrite(LED1, brightness1);
-    analogWrite(LED2, brightness2);
-    analogWrite(LED3, brightness3);
-    // change the brightness for next time through the loop:
-    brightness1 = brightness1 + fadeAmount1;
-    brightness2 = brightness2 + fadeAmount2;
-    brightness3 = brightness3 + fadeAmount3;
-    // reverse the direction of the fading at the ends of the fade: 
-    if (brightness1 <= 0 || brightness1 >= 250) {
-      fadeAmount1 = -fadeAmount1 ; 
-    }     
-    if (brightness2 <= 0 || brightness2 >= 250) {
-      fadeAmount2 = -fadeAmount2 ; 
-    }     
-    if (brightness3 <= 0 || brightness3 >= 250) {
-      fadeAmount3 = -fadeAmount3 ; 
-    }        
-    delay(fadeTable[wait_index()]);                
-}
-
-int wait_index( void ){//iterates through the fade table
-  return (di = ++di %FADE_LENGTH); 
+uint16_t calculate_flow_rate(uint16_t pulses){
+    return (pulses/TIMER_PERIOD); 
 }
 
 
