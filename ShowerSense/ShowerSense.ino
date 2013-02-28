77c2e934f77b29e433a3be474bef343becf1614e
Sean Connell <seanrobertconnell@gmail.com>
2013-02-28 01:58:03 -0800

    Basic Functionality
    Currently counts for some number of user defined seconds with a
    timer/variable and reports flow rate in ml every timesclice.

--- ShowerSense/ShowerSense.ino
+++ ShowerSense/ShowerSense.ino
@@ -14,19 +14,23 @@ power on init-> DATA_GATHERING-----<timer overflow>----[-> DATA_TRANSFORM]----<t
                     |                                                                  |  
                     |---------------------------------[CLEAR_STATE <-]---------<notify completes>
                     
-TODO: Add in timer stop/start and watchdog timer initialize/cleanup and use to reset
+TODO: Add in watchdog timer initialize/cleanup and use to reset
+TODO: Put MCU to sleep while not gathering data via interrupts
      
 */
 
-#define IN_USE_FLOW_RATE 50
-#define TIMER_PERIOD 1000
-#define SHOWER_IN_USE(X) (return X > IN_USE_FLOW_RATE)
+//User Settings
 #define FLOW_SENSOR_PIN 2
+//Be careful about setting this, it might overflow variables tracking various data if too long
+#define REPORT_INTERVAL_SECONDS 60
+
+//Readability defines
 #define CLEAR_PENDING_INTERRUPTS EIFR = 0
 #define ENABLE_INTERRUPTS sei()
 #define DISABLE_INTERRUPTS cli()
 #define STOP_TIMER TCCR1B &= 0B11111000 //Clear CS12,11,10 I think this is clearer than doing the shift over invert thing
-#define START_TIMER TCCR1B |= (1 << CS10) | (1 << CS12); //Set 1024 prescaler bits
+#define CLEAR_TIMER TCNT1 = 0B00000000
+#define START_TIMER TCCR1B |= (1 << CS10) | (1 << CS12); //Set prescaler to 1024, and start timer
 
 //States for the system
 #define INITIAL         B00000000
@@ -38,8 +42,8 @@ TODO: Add in timer stop/start and watchdog timer initialize/cleanup and use to r
 
 //Volatile stateful variables
 volatile uint16_t  pulse_count;
-volatile uint8_t STATE = INITIAL;
-
+volatile uint8_t   STATE = INITIAL;
+volatile uint8_t   second_counter = 0;
 //Data to be transformed register
 #define FLOW_RATE_DATA  B00000001
 #define SALINITY_DATA   B00000010
@@ -49,30 +53,39 @@ volatile uint8_t STATE = INITIAL;
 uint16_t flow_rate = 0;
 
 //The data gathering window has ended and we should move on to calculating reporting values
-ISR(TIMER1_COMPA_vect){
-  if(STATE != DATA_GATHERING){
-    /* We somehow got to the end of our data gathering window without resetting state
-    Might be a bad calc somewhere in the DATA_TRANSFORM step
-    Try to restart and hope that it doesn't happen again */ 
-    STATE = RESTART; 
-    DISABLE_INTERRUPTS;//We're going to bail out and restart. Don't need to worry about the world
-  }
-  else{
-    STATE = DATA_TRANSFORM;
-    STOP_TIMER;
-    DISABLE_INTERRUPTS;//Don't want to collect any data while we copy some values
+ISR(TIMER1_COMPA_vect){  
+  second_counter++;
+  
+  //wait to transition state until we hit the end of our observation window
+  if(REPORT_INTERVAL_SECONDS == second_counter){
+    second_counter = 0; 
+    
+    if(STATE != DATA_GATHERING){
+      /* We somehow got to the end of our data gathering window without resetting state
+      Might be a bad calc somewhere in the DATA_TRANSFORM step
+      Try to restart and hope that it doesn't happen again */ 
+      STOP_TIMER;
+      STATE = RESTART; 
+      DISABLE_INTERRUPTS;//We're going to bail out and restart. Don't need to worry about the world
+    }
+    
+    else{
+      STOP_TIMER;
+      STATE = DATA_TRANSFORM;
+      DISABLE_INTERRUPTS;//Don't want to collect any data while we copy some values
+    }
   }
 }
 
 void setup() {
    clear_watchdog();
    Serial.begin(115200); //initialize uart
-   pinMode(FLOW_SENSOR_PIN, INPUT); 
-   digitalWrite(FLOW_SENSOR_PIN, HIGH);
-   attachInterrupt(FLOW_SENSOR_PIN, pulses_counter, CHANGE);
+   Serial.println("Initializing...");
+   attachInterrupt(0, pulses_counter, CHANGE);
    initialize_timer();
    STATE = DATA_GATHERING;
    ENABLE_INTERRUPTS;
+   START_TIMER;
 }
 
 void loop()
@@ -103,22 +116,24 @@ void loop()
     //Notify whatever is listening by whatever method
     case NOTIFY_LISTENER:
       Serial.print("Flow Rate:\t"); 
-      Serial.println(flow_rate);
+      Serial.print(flow_rate);
+      Serial.println("\t[ml]");
       STATE = CLEAR_STATE;
       break;
     
     //reset everything to be ready for the next cycle
     case CLEAR_STATE:
       flow_rate = 0;
+      pulse_count = 0;
       CLEAR_PENDING_INTERRUPTS;
       STATE = DATA_GATHERING;
+      CLEAR_TIMER;
       START_TIMER;
       ENABLE_INTERRUPTS;
       break;
       
     //currently hard coded error message for reset. Generalize later
     case RESTART:
-      Serial.println("Reset not yet implemented. Would if it could though...");
       Serial.println("|-------------------------------------RESETTING------------------------------------|"); 
       Serial.println("| ILLEGAL STATE: Did not return to DATA_GATHERING state before time window elapsed |"); 
       Serial.println("|-------------------------------------RESETTING------------------------------------|"); 
@@ -137,17 +152,25 @@ void pulses_counter(){
 void initialize_timer(){
   TCCR1A = 0;
   TCCR1B = 0;
-  OCR1A = 46874; //compare mode with 3 second intervals at this clock rate of 16mhz
+  OCR1A = 15624; //compare mode with 1 second intervals at this clock rate of 16mhz and prescale of 1024
   TCCR1B |= (1 << WGM12); //CTC mode
   TIMSK1 |= (1 << OCIE1A); //enable timer compare interrupt
 }
 
+/*2.25 ml per pulse rougly speaking
+ so *(9/4), div 4 of course being 2 
+ bitshifts right. Not sure if compiler
+ is smart enough to know that though*/
 uint16_t calculate_flow_rate(uint16_t pulses){
-    return (pulses/TIMER_PERIOD); 
+    return ((pulses*9)>>2); 
 }
 
 void clear_watchdog(){
     //TODO
+}
+
+void initialize_watchdog(){
+   //TODO 
 }
 
 void reset(){
