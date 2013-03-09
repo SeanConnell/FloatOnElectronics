c04ccd462bb768fe193943aa567f8e7ff085c12d
Sean Connell <seanrobertconnell@gmail.com>
2013-03-09 02:12:01 -0600

    Moving along with basic printing and error handling

--- RaspberryPiScripts/helm_reporter.py
+++ RaspberryPiScripts/helm_reporter.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 import serial
+import json
+
+device = '/dev/ttyACM0' #probably should detect this instead of hardcode
+baud_rate = 38400 #higher baud rates make things break on this computer at least
+timeout = 10 #10 second timeout
 
 #Open our connection on the usual place
-ser = serial.Serial('/dev/ttyACM0',115200, timeout=10)
+ser = serial.Serial(device, baud_rate, timeout=timeout)
 #do a hardware reset on a yet to be implemented command 
 while True:
-    print ser.readline()
+    data = ser.readline()
+    try:
+        jdata = json.loads(data)
+    except ValueError:
+        print "invalid json, moving on."
+        print data
+        continue;
+    print jdata
+
